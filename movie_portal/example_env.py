b0a5fa64a8a4e4ef1cf7dc22d0789e7b33453192
Tusamma Sal Sabil <tssovi@gmail.com>
2020-03-01 16:15:26 +0600

    Initial Commit

---
+++ movie_portal/example_env.py
@@ -0,0 +1,10 @@
+SECRET_KEY = 'gi8^_2bk+83r^v+x#y2xfife3(eli9cj!a1+f%)qjxd1u)%c5@'
+ALLOWED_HOSTS = ['*']
+DEBUG = False
+
+ENGINE = 'django.db.backends.sqlite3'
+NAME = "db.sqlite3"
+USER = ''
+PASSWORD = ''
+HOST = ''
+PORT = ''
