2533faf712f0efa6ba3682fcfd28b64ae4e2d4c8
Tusamma Sal Sabil <7576130+tssovi@users.noreply.github.com>
2020-03-23 16:46:56 +0600

    Update example_env.py

--- movie_portal/example_env.py
+++ movie_portal/example_env.py
@@ -1,10 +1,18 @@
-SECRET_KEY = 'gi8^_2bk+83r^v+x#y2xfife3(eli9cj!a1+f%)qjxd1u)%c5@'
+SECRET_KEY = 'f6zq+&)v6%w6ik*3v)zp%e=m+c@-9i8yc&vmp1f293@h=+jf+9'
 ALLOWED_HOSTS = ['*']
 DEBUG = False
 
-DATABASE_ENGINE = 'django.db.backends.postgresql_psycopg2'
-DATABASE_NAME = 'test_db'
-DATABASE_USER = 'test_db_username'
-DATABASE_PASSWORD = 'test_db_password'
+DATABASE_ENGINE = 'django.db.backends.mysql'
+DATABASE_NAME = 'database_name'
+DATABASE_USER = 'db_username'
+DATABASE_PASSWORD = 'db_password'
 DATABASE_HOST = 'localhost'
-DATABASE_PORT = '5432'
+DATABASE_PORT = '3302'
+CHARTSET = 'utf8mb4'
+INIT_COMMAND = "ALTER DATABASE {} CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci;".format(DATABASE_NAME)
+
+YOUTUBE_API_KEY = 'your_secret_api_key'
+
+# Data Update Interval In Seconds
+DATA_UPDATE_INTERVAL = 1800
+
